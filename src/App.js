ba09387992109ae4d06c5ba281f1f15b622c96f3
devansh0627 <101563736+devansh0627@users.noreply.github.com>
2024-06-07 22:05:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// src/App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Container, CssBaseline, IconButton } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -9,7 +8,8 @@ import PostList from './components/PostList';
 import './index.css'; // Ensure you import your custom styles
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);// state which defines whether it's dark mode or not 
+  // Retrieve the dark mode state from local storage, defaulting to false if it's not set
+  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') === 'true' || false);
 
   const theme = createTheme({
     palette: {
@@ -17,10 +17,18 @@ const App = () => {
     },
   });
 
-  
-  const handleThemeChange = () => {// function to switch between light and dark mode
-    setDarkMode(!darkMode);
+  const handleThemeChange = () => {
+    // Toggle the dark mode state
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    // Store the new dark mode state in local storage
+    localStorage.setItem('darkMode', newDarkMode);
   };
+
+  useEffect(() => {
+    // Set the theme mode when the component mounts
+    document.documentElement.setAttribute('data-theme', theme.palette.mode);
+  }, [theme.palette.mode]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -38,8 +46,7 @@ const App = () => {
         </AppBar>
         <Container className="scrollbar mt-8">
           <Routes>
-          <Route path="/" element={<PostList darkMode={darkMode} />} />
-
+            <Route path="/" element={<PostList darkMode={darkMode} />} />
           </Routes>
         </Container>
       </Router>
